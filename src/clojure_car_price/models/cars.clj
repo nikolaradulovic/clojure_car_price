c7e66f8144bfa968ca1f45c28b5c2c5f75c48e34
nikolaradulovic <nikola.radulovic_bgd@hotmail.com>
2015-09-10 09:14:18 +0200

    Update cars.clj

--- src/clojure_car_price/models/cars.clj
+++ src/clojure_car_price/models/cars.clj
@@ -1,13 +1,13 @@
 (ns clojure_car_price.models.cars
   (:refer-clojure :exclude [get])
   (:require [clojure.java.jdbc :as j]
-				[clojure.java.jdbc.sql :as s]))
+	    [clojure.java.jdbc.sql :as s]))
 
 (def mysql-db {:subprotocol "mysql"
-                     :subname "//localhost:3306/carsdb"
-                     :user "root"
-                     :pass " "
-                     :zeroDateTimeBehaviour "convertToNull"})
+               :subname "//localhost:3306/carsdb"
+               :user "root"
+               :pass " "
+               :zeroDateTimeBehaviour "convertToNull"})
 
 (def now
   (str (java.sql.Timestamp. (System/currentTimeMillis))))
