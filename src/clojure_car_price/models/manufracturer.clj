e591c2da99099558bedbeeee1a18a538eb20259f
nikolaradulovic <nikola.radulovic_bgd@hotmail.com>
2015-09-10 09:18:34 +0200

    Update manufracturer.clj

--- src/clojure_car_price/models/manufracturer.clj
+++ src/clojure_car_price/models/manufracturer.clj
@@ -1,13 +1,13 @@
 (ns clojure_car_price.models.manufracturer
   (:refer-clojure :exclude [get])
   (:require [clojure.java.jdbc :as j]
-				[clojure.java.jdbc.sql :as s]))
+	    [clojure.java.jdbc.sql :as s]))
 
 (def mysql-db {:subprotocol "mysql"
-					 :subname "//localhost:3306/carsdb"
-					 :user "root"
-					 :pass " "
-					 :zeroDateTimeBehaviour "convertToNull"})
+	       :subname "//localhost:3306/carsdb"
+	       :user "root"
+	       :pass " "
+	       :zeroDateTimeBehaviour "convertToNull"})
 
 (defn get [id]
   (first (j/query mysql-db
