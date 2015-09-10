ec6e75b29a029a85b272571358b76089e29243ba
nikolaradulovic <nikola.radulovic_bgd@hotmail.com>
2015-09-10 09:15:52 +0200

    Update country.clj

--- src/clojure_car_price/models/country.clj
+++ src/clojure_car_price/models/country.clj
@@ -1,13 +1,13 @@
 (ns clojure_car_price.models.country
   (:refer-clojure :exclude [get])
   (:require [clojure.java.jdbc :as j]
-				[clojure.java.jdbc.sql :as s]))
+	    [clojure.java.jdbc.sql :as s]))
 
 (def mysql-db {:subprotocol "mysql"
-					 :subname "//localhost:3306/carsdb"
-					 :user "root"
-					 :pass ""
-					 :zeroDateTimeBehaviour "convertToNull"})
+	       :subname "//localhost:3306/carsdb"
+	       :user "root"
+	       :pass ""
+	       :zeroDateTimeBehaviour "convertToNull"})
 
 (defn allCountries []
   (j/query mysql-db
