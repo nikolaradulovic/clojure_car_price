5dc61bfdfb568d7d8aec06756884670d06416b57
nikolaradulovic <nikola.radulovic_bgd@hotmail.com>
2015-09-10 09:15:09 +0200

    Update city.clj

--- src/clojure_car_price/models/city.clj
+++ src/clojure_car_price/models/city.clj
@@ -1,13 +1,13 @@
 (ns clojure_car_price.models.city
   (:refer-clojure :exclude [get])
   (:require [clojure.java.jdbc :as j]
-				[clojure.java.jdbc.sql :as s]))
+	    [clojure.java.jdbc.sql :as s]))
 
 (def mysql-db {:subprotocol "mysql"
-					 :subname "//localhost:3306/carsdb"
-					 :user "root"
-					 :pass " "
-					 :zeroDateTimeBehaviour "convertToNull"})
+	       :subname "//localhost:3306/carsdb"
+	       :user "root"
+	       :pass " "
+	       :zeroDateTimeBehaviour "convertToNull"})
 
 (defn allCities []
   (j/query mysql-db
